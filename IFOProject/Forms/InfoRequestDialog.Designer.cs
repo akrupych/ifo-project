2816bb1a7e07865552848d7ef4c5e9e4ac81609e
andriy.krupych@gmail.com <andriy.krupych@gmail.com@9ac41e86-6e5b-211f-1c19-719326da811c>
2013-09-12 00:24:01 +0000

    - Implemented Results Export - Added InfoRequestDialog

---
+++ IFOProject/Forms/InfoRequestDialog.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace IFOProject.Forms
+{
+    partial class InfoRequestDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCrystalMaterial = new System.Windows.Forms.TextBox();
+            this.textBoxCut = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLoadDirection = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please provide some additional information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Crystal material:";
+            // 
+            // textBoxCrystalMaterial
+            // 
+            this.textBoxCrystalMaterial.Location = new System.Drawing.Point(99, 34);
+            this.textBoxCrystalMaterial.Name = "textBoxCrystalMaterial";
+            this.textBoxCrystalMaterial.Size = new System.Drawing.Size(121, 20);
+            this.textBoxCrystalMaterial.TabIndex = 2;
+            // 
+            // textBoxCut
+            // 
+            this.textBoxCut.Location = new System.Drawing.Point(99, 60);
+            this.textBoxCut.Name = "textBoxCut";
+            this.textBoxCut.Size = new System.Drawing.Size(121, 20);
+            this.textBoxCut.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cut (X, Y, Z):";
+            // 
+            // textBoxLoadDirection
+            // 
+            this.textBoxLoadDirection.Location = new System.Drawing.Point(99, 86);
+            this.textBoxLoadDirection.Name = "textBoxLoadDirection";
+            this.textBoxLoadDirection.Size = new System.Drawing.Size(121, 20);
+            this.textBoxLoadDirection.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Load direction:";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(120, 112);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(100, 23);
+            this.buttonCancel.TabIndex = 8;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(12, 112);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(100, 23);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // InfoRequestDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 150);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxLoadDirection);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxCut);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCrystalMaterial);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "InfoRequestDialog";
+            this.Text = "Additional info";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InfoRequestDialog_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCrystalMaterial;
+        private System.Windows.Forms.TextBox textBoxCut;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxLoadDirection;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
---
+++ IFOProject/Forms/InfoRequestDialog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IFOProject.Forms
+{
+    /// <summary>
+    /// Requests user for additional info when exporting results.
+    /// Info is stored in dialog fields and can be accessed after dialog is closed.
+    /// </summary>
+    public partial class InfoRequestDialog : Form
+    {
+        // fields for storing results
+        // can be accessed after dialog is closed
+        public string CrystalMaterial { get; private set; }
+        public string Cut { get; private set; }
+        public string LoadDirection { get; private set; }
+
+        public InfoRequestDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+            Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            Close();
+        }
+
+        /// <summary>
+        /// Save results when form is closing
+        /// </summary>
+        private void InfoRequestDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CrystalMaterial = textBoxCrystalMaterial.Text;
+            Cut = textBoxCut.Text;
+            LoadDirection = textBoxLoadDirection.Text;
+        }
+    }
+}
